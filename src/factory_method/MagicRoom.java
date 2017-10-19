b8e5a68b6e66202bdf0403b2be9611732339e16c
Rohit <mourya.rohit32@gmail.com>
2017-10-19 22:20:02 +0530

    Understanding factory method pattern

--- src/factory_method/MagicRoom.java
+++ src/factory_method/MagicRoom.java
@@ -1,4 +1,7 @@
 package factory_method;
 
 public class MagicRoom extends Room {
+    public MagicRoom () {
+        System.out.println("In Magic Room");
+    }
 }
--- src/factory_method/OrdinaryRoom.java
+++ src/factory_method/OrdinaryRoom.java
@@ -1,4 +1,7 @@
 package factory_method;
 
 public class OrdinaryRoom extends Room {
+    public OrdinaryRoom () {
+        System.out.println("In Ordinary Room");
+    }
 }
